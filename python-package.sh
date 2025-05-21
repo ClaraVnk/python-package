0897e4230db2593b2a799e4766ee555c2c5c9a20
claravnk <claravanacker27@gmail.com>
2025-05-21 13:36:17 +0200

    creation

---
+++ python-package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e  # stop on error
+
+echo "🧹 Nettoyage des anciens builds..."
+rm -rf dist/*
+
+echo "🛠️ Construction du package..."
+python -m build
+
+echo -n "🔐 Veuillez saisir votre token API PyPI (entrée cachée) : "
+read -s PYPI_API_TOKEN
+echo
+
+if [ -z "$PYPI_API_TOKEN" ]; then
+  echo "❌ Token API PyPI non fourni."
+  exit 1
+fi
+
+echo "🚀 Upload sur PyPI avec twine..."
+python3 -m twine upload dist/* -u __token__ -p "$PYPI_API_TOKEN"
+
+echo "✅ Package publié avec succès !"
